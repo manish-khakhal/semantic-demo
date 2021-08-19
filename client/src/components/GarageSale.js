4e012860fdff760c0e036f8dca6049adba1d7364
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2021-08-18 20:18:23 -0700

    commiting trish-gui branchwork

--- client/src/components/GarageSale.js
+++ client/src/components/GarageSale.js
@@ -8,12 +8,34 @@ function GarageSale() {
       <br></br>
       <div>
         <div class="ui fluid container">
-          <div class="ui fluid grid">
+          <div class="ui grid">
             <div class="row">
               <br></br>
               <div class="one wide column"></div>
 
               <div class="five wide column">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  
                 <h1>Search Garage Sale Items</h1>
 
                 <div class="ui card">
@@ -24,19 +46,23 @@ function GarageSale() {
                     <div class="header">A Sale Item</div>
                     <div class="meta">
                       <span class="date">keyword, keyword, keyword</span>
+                     
                     </div>
 
-                    <br></br>
+                
+<br></br>
                     <Button color="olive">Add to Shopping Cart</Button>
+
+
+                    
                   </div>
                 </div>
-
-
-                
               </div>
             </div>
           </div>
         </div>
+
+
         <br></br> <br></br>
       </div>
     </div>

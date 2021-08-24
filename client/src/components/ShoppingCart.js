297b5f695d66035eb89ce272a97dddfba0fc58f0
augustinandrea <augustin.andrea@gmail.com>
2021-08-23 19:51:49 -0700

    added stripe

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
+//import { loadStripe } from "@stripe/stripe-js";
+
+//const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 function ShoppingCart() {
+
   return (
     <div className="App">
        <h2> this is the ShoppingCart</h2>

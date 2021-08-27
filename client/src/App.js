dca62a5851f9c7e1b46d3939d8783ce4e34ba894
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2021-08-26 20:42:49 -0700

    fixed one error/landed on another

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,21 @@ import ShoppingCart from './components/ShoppingCart.js';
 
 import 'semantic-ui-css/semantic.min.css';
 import Footer from './components/Footer.js';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
 
-
-
-
-
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
+  uri: '/graphql',
+});
 
 
 
@@ -38,30 +47,21 @@ function App() {
   )
   
 
-    return (
+  return (
+    //<ApolloProvider client={client}>
       <div className="App">
+        <ApolloProvider client={client}>
+          <NavBar pages={pages} setPage={setPage}/>
+          {page}
+          <Footer />
+        </ApolloProvider>
 
-
-  
-  <NavBar pages={pages} setPage={setPage}/>
-
-  
-  
-  {page}
-  
-
-  <Footer />
-       
       </div>
+    //</ApolloProvider>
 
-
-
-
-
-
-    );
-  }
+   );
+}
   
-  export default App;
+export default App;
   
   
